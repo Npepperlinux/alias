aad5b75ad38963495fff71220a0b0797e0438e22
Npepperlinux <alterlinuxer@gmail.com>
2022-03-23 20:23:16 +0900

    add short.sh

---
+++ short.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+alias 更新="sudo apt update;sudo apt upgrade"
+alias 端末を閉じる="exit"
+alias 再起動="reboot"
+alias BIOSに移動="sudo systemctl reboot --firmware-setup"
+
+alias Chrome="google-chrome"
+alias Vivaldi="vivaldi"
+alias Chromium="chromium-browser"
+alias Edge="microsoft-edge"
+alias Firefox="firefox"
+alias VScode="code"
+
+alias Eメール="xdg-email"
